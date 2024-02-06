cb490e40a9aacbbb942e50fe30000857a93da9d8
Provat <provat@capitalnumbers.com>
2024-02-06 16:30:53 +0530

    Removed CSS related to Swiper slider Ran through WPCS for features.php

--- template-parts/features.php
+++ template-parts/features.php
@@ -4,17 +4,18 @@
  *
  * @package PLK
  */
-$group = $sub ?? null ?: $args['group'] ?? null;
+
+$group         = $sub ?? null ?: $args['group'] ?? null;
 $args['group'] = $group;
 
-$title = $group['title'] ?? null;
-$items = $group['items'] ?? null;
+$block_title    = $group['title'] ?? null;
+$items          = $group['items'] ?? null;
 $bottom_content = $group['bottom_content'] ?? null;
-$top_content = $group['top_content'] ?? null;
+$top_content    = $group['top_content'] ?? null;
 
 $top_large_padding = $group['top_large_padding'] ?? null;
-$bottom_padding = $group['bottom_padding'] ?? null;
-$gradient_icons = $group['gradient-icons'] ?? null;
+$bottom_padding    = $group['bottom_padding'] ?? null;
+$gradient_icons    = $group['gradient-icons'] ?? null;
 
 $class = 'feature section-padding-top bg--gray-gradient';
 if ($top_large_padding) {
@@ -28,49 +29,50 @@ if ($gradient_icons) {
 }
 ?>
 
-<section class="<?php echo trim($class); ?>">
+<section class="<?php echo esc_html($class); ?>">
     <div class="ast-container">
         <div class="container">
-            <?php if ($title) { ?>
-                <h2 class="heading-2"><?php echo $title ?></h2>
-            <?php } ?>
-            <?php if ($top_content) { ?>
-                <p class="feature__caption"><?php echo $top_content ?></p>
-            <?php } ?>
-            <?php if ($items) { ?>
+            <?php if ($block_title) : ?>
+                <h2 class="heading-2"><?php echo esc_html($block_title); ?></h2>
+            <?php endif; ?>
+            <?php if ($top_content) : ?>
+                <p class="feature__caption"><?php echo esc_html($top_content); ?></p>
+            <?php endif; ?>
+            <?php if ($items) : ?>
                 <div class="feature__slider custom-navigation splide">
                     <div class="feature-container splide__track">
                         <ul class="feature__list list-reset splide__list">
-                            <?php foreach ($items as $item) {
-                                $iIcon = $item['icon'] ?? null;
-                                $iTitle = $item['title'] ?? null;
-                                $iSubtitle = $item['subtitle'] ?? null;
+                            <?php 
+                            foreach ( $items as $item ) {
+                                $i_icon     = $item['icon'] ?? null;
+                                $i_title    = $item['title'] ?? null;
+                                $i_subtitle = $item['subtitle'] ?? null;
                                 ?>
                                 <li class="splide__slide">
                                     <div class="feature__item">
-                                        <?php if ($iIcon) { ?>
+                                        <?php if ($i_icon) : ?>
                                             <div class="feature__item-icon">
                                                 <picture>
-                                                    <?php echo wp_get_attachment_image($iIcon['ID'], 'medium', '', ['alt' => $iIcon['alt'] ?: $iTitle]); ?>
+                                                    <?php echo wp_get_attachment_image( $i_icon['ID'], 'medium', '', array ( 'alt' => $i_icon['alt'] ?: $i_title ) ); ?>
                                                 </picture>
                                             </div>
-                                        <?php } ?>
-                                        <?php if ($iTitle) { ?>
-                                            <h3 class="heading-4 feature__item-name"><?php echo $iTitle ?></h3>
-                                        <?php } ?>
-                                        <?php if ($iSubtitle) { ?>
-                                            <p class="text-medium item__item-caption"><?php echo $iSubtitle ?></p>
-                                        <?php } ?>
+                                        <?php endif; ?>
+                                        <?php if ($i_title) : ?>
+                                            <h3 class="heading-4 feature__item-name"><?php echo esc_html($i_title); ?></h3>
+                                        <?php endif; ?>
+                                        <?php if ($i_subtitle) : ?>
+                                            <p class="text-medium item__item-caption"><?php echo esc_html($i_subtitle); ?></p>
+                                        <?php endif; ?>
                                     </div>
                                 </li>
                             <?php } ?>
                         </ul>
                     </div>
-                    <?php if ($bottom_content) { ?>
-                        <p class="feature__caption"><?php echo $bottom_content ?></p>
-                    <?php } ?>
+                    <?php if ($bottom_content) : ?>
+                        <p class="feature__caption"><?php echo esc_html($bottom_content); ?></p>
+                    <?php endif; ?>
                 </div>
-            <?php } ?>
+            <?php endif; ?>
         </div>
     </div>
 </section>
