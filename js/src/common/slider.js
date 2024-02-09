51668c1b07cb78959588ffe8f1e47e6a433faa66
Provat <provat@capitalnumbers.com>
2024-02-09 12:46:02 +0530

    Removed 1 rem margin from heading, adjusted slider trackwidth, padding

--- js/src/common/slider.js
+++ js/src/common/slider.js
@@ -7,6 +7,7 @@ export const splideSlider = (className, breakPoints = STATIC_PAGES_SLIDER_BREAKP
     perPage: 6,
     perMove: 1,
     breakpoints: breakPoints,
+    padding: { left: 0, right: 5 },
   } );
   splide.mount();
 }
