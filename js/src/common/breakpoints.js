382abb22918c0267e20c18ae9f87564499c91850
Provat <provat@capitalnumbers.com>
2024-02-12 13:20:18 +0530

    Fixd font size Fixed breakpoints Fixed left margin for heading and link fixed navigation arrwos for breakpoint 1040 and below of it Fixed add-to-cart button alignment

--- js/src/common/breakpoints.js
+++ js/src/common/breakpoints.js
@@ -13,41 +13,22 @@ export const STATIC_PAGES_SLIDER_BREAKPOINTS = {
     }
 };
 
+
+// Swiper configuration for product sliders
 export const PRODUCT_BY_CATEGORY_SLIDER_BREAKPOINTS = {
-    400: {
+    480: {
         perPage: 2,
     },
     768: {
         perPage: 3,
     },
     1024: {
-        perPage: 5,
-    },
-    1282: {
-        perPage: 6,
-    }
-};
-
-// Swiper configuration for product sliders
-export const PRODUCT_SLIDER_BREAKPOINTS = {
-    360: {
-        slidesPerView: 2,
-        spaceBetween: 14
-    },
-    768: {
-        slidesPerView: 3,
-        spaceBetween: 14
-    },
-    1024: {
-        slidesPerView: 4,
-        spaceBetween: 14
+        perPage: 4,
     },
     1280: {
-        slidesPerView: 4,
-        spaceBetween: 14
+        perPage: 5,
     },
     1440: {
-        slidesPerView: 4,
-        spaceBetween: 30
+        perPage: 6,
     }
 };
