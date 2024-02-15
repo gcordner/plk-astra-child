bb020dd6f53fcf501e2b95b1ce29d366ad4c114b
Provat <provat@capitalnumbers.com>
2024-02-15 19:54:29 +0530

    Added splide slider for vien info block, adjusting the CSS for the block.

---
+++ js/src/components/vien.js
@@ -0,0 +1,4 @@
+import { splideSlider } from '../common/slider';
+import { STATIC_PAGES_SLIDER_BREAKPOINTS } from '../common/breakpoints';
+
+export const vienSlider = () => splideSlider('#vien_splide', STATIC_PAGES_SLIDER_BREAKPOINTS);
--- template-parts/info_cards.php
+++ template-parts/info_cards.php
@@ -7,7 +7,7 @@
  * @package info
  */
 
-$sub = get_sub_field(pathinfo(__FILE__, PATHINFO_FILENAME));
+$sub   = get_sub_field(pathinfo(__FILE__, PATHINFO_FILENAME));
 $group = $sub ? $sub : ( $args['group'] ? $args['group'] : null );
 //Sub Init Done
 
@@ -15,63 +15,65 @@ $title = $args['group']['title'] ?? null;
 $image = $args['group']['image'] ?? null;
 $cards = $args['group']['cards'] ?? null;
 ?>
-<section class="vien section-padding background--gray">
-    <div class="container">
-        <?php if ($title) { ?>
-            <h2 class="heading-2 vien__heading"><?php echo $title ?></h2>
-        <?php } ?>
-        <?php if ($cards) { ?>
-            <div class="vien__slider custom-navigation">
-                <div class="swiper-container">
-                    <ul class="vien__list list-reset swiper-wrapper">
-                        <?php foreach ($cards as $card) {
-                            $cImage = $card['image'] ?? null;
-                            $cTitle = $card['title'] ?? null;
-                            $cItems = $card['items'] ?? null;
-                            $cLink = $card['link'] ?? null;
-                            $cColor = $card['color'] ?? null;
-                            ?>
-                            <li class="swiper-slide">
-                                <div class="vien__item">
-                                    <?php if ($cImage) { ?>
-                                        <picture>
-                                            <?php echo wp_get_attachment_image($cImage['ID'], 'medium', '', ['alt' => $cImage['alt'] ?: $cTitle]); ?>
-                                        </picture>
-                                    <?php } ?>
-                                    <?php if ($cTitle) { ?>
-                                        <h3 class="heading-4 vien__item-title"><?php echo $cTitle ?></h3>
-                                    <?php } ?>
-                                    <?php if ($cItems) { ?>
-                                        <ul class="vien__item-ul list-reset <?php echo $cColor ?>">
-                                            <?php foreach ($cItems as $cItem) {
-                                                $vIcon = $cItem['icon'] ?? null;
-                                                $vName = $cItem['name'] ?? null;
-                                                ?>
-                                                <li class="vien__item-li">
-                                                    <?php if ($vIcon) { ?>
-                                                        <span class="vien__item-li-icon <?php echo $vIcon ?>"></span>
-                                                    <?php } ?>
-                                                    <?php if ($vName) { ?>
-                                                        <p class="vien__item-li-name nav-text"><?php echo $vName ?></p>
-                                                    <?php } ?>
-                                                </li>
-                                            <?php } ?>
-                                        </ul>
-                                    <?php } ?>
-                                    <?php if ($cLink) {
-                                        $link = new Link($cLink);
-                                        $link->class = 'btn btn--transparent vien__item-link';
-                                        $link->wrapper_end = '<span class="icon-nav-arrow"></span>';
-                                        echo $link->a();
-                                    }?>
-                                </div>
-                            </li>
-                        <?php } ?>
-                    </ul>
-                    <div class="custom-navigation__button custom-navigation__button--next custom-navigation__button--next-vien"><span class="icon-arrow-up"></span></div>
-                    <div class="custom-navigation__button custom-navigation__button--prev custom-navigation__button--prev-vien"><span class="icon-arrow-up"></span></div>
+<section class="vien section-padding background--gray section-margin">
+    <div class="ast-container">
+        <div class="container">
+            <?php if ($title) : ?>
+                <h2 class="heading-2 vien__heading"><?php echo $title ?></h2>
+            <?php endif; ?>
+            <?php if ($cards) : ?>
+                <div class="vien__slider custom-navigation splide" id="vien_splide">
+                    <div class="vien-container splide__track">
+                        <ul class="vien__list list-reset splide__list">
+                            <?php foreach ($cards as $card) {
+                                $cImage = $card['image'] ?? null;
+                                $cTitle = $card['title'] ?? null;
+                                $cItems = $card['items'] ?? null;
+                                $cLink  = $card['link'] ?? null;
+                                $cColor = $card['color'] ?? null;
+                                ?>
+                                <li class="splide__slide">
+                                    <div class="vien__item">
+                                        <?php if ($cImage) { ?>
+                                            <picture>
+                                                <?php echo wp_get_attachment_image($cImage['ID'], 'medium', '', ['alt' => $cImage['alt'] ?: $cTitle]); ?>
+                                            </picture>
+                                        <?php } ?>
+                                        <?php if ($cTitle) { ?>
+                                            <h3 class="heading-4 vien__item-title"><?php echo $cTitle ?></h3>
+                                        <?php } ?>
+                                        <?php if ($cItems) { ?>
+                                            <ul class="vien__item-ul list-reset <?php echo $cColor ?>">
+                                                <?php foreach ($cItems as $cItem) {
+                                                    $vIcon = $cItem['icon'] ?? null;
+                                                    $vName = $cItem['name'] ?? null;
+                                                    ?>
+                                                    <li class="vien__item-li">
+                                                        <?php if ($vIcon) { ?>
+                                                            <span class="vien__item-li-icon <?php echo $vIcon ?>"></span>
+                                                        <?php } ?>
+                                                        <?php if ($vName) { ?>
+                                                            <p class="vien__item-li-name nav-text"><?php echo $vName ?></p>
+                                                        <?php } ?>
+                                                    </li>
+                                                <?php } ?>
+                                            </ul>
+                                        <?php } ?>
+                                        <?php 
+                                        if ($cLink) :
+                                            $nlink = new Link($cLink);
+                                            $nlink->class = 'btn btn--transparent vien__item-link';
+                                            $nlink->wrapper_end = '<span class="icon-nav-arrow"></span>';
+                                            echo $nlink->a();
+                                        endif;
+                                        ?>
+                                    </div>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        <?php } ?>
+            <?php endif; ?>
+        </div>
     </div>
 </section>
