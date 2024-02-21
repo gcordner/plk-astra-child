b00837d31bd22d1f1497be3d9c6b6788b0154da2
Provat <provat@capitalnumbers.com>
2024-02-21 20:11:49 +0530

    Fixed WPCS

--- template-parts/faq.php
+++ template-parts/faq.php
@@ -1,43 +1,66 @@
 <?php
-$title = $args['group']['title'] ?? null;
+/**
+ * FAQ Block
+ *
+ * @package plk-child-theme
+ */
+
+$faq_title   = $args['group']['title'] ?? null;
 $heading_tag = $args['group']['heading_tag'] ?? null;
-$items = $args['group']['items'] ?? null;
+$items       = $args['group']['items'] ?? null;
 ?>
-<?php if ($items) : ?>
+<?php if ( $items ) : ?>
 <section class="faq section-padding background--gray faq--page"
-    <?php 
-    if ( !is_page( 104524 ) ) : 
-        ?> itemscope itemtype="https://schema.org/FAQPage"
-        <?php 
-    endif; 
-    ?>>
+    <?php
+    if ( ! is_page( 104524 ) ) :
+        ?>
+        itemscope itemtype="https://schema.org/FAQPage"
+        <?php
+    endif;
+    ?>
+    >
     <div class="ast-container">
         <div class="container">
             <div class="faq__inner">
-                <?php if ($title && $heading_tag) {
-                    echo '<' . $heading_tag . ' class="heading-2 faq__heading">' . $title . '</' . $heading_tag . '>';
-                } ?>
+                <?php
+                if ( $faq_title && $heading_tag ) :
+                    echo '<' . esc_html( $heading_tag ) . ' class="heading-2 faq__heading">' . esc_html( $faq_title ) . '</' . esc_html( $heading_tag ) . '>';
+                endif;
+                ?>
                 <div class="faq__list">
-                    <?php foreach ($items as $item) {
+                    <?php
+                    foreach ( $items as $item ) {
                         $question = $item['question'];
-                        $answer = $item['answer'];;
+                        $answer   = $item['answer'];
                         ?>
                         <div class="faq__item accordion" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
-                            <?php if ($question) { ?>
+                            <?php
+                            if ( $question ) :
+                                ?>
                                 <div class="faq__item-link accordion__link">
-                                    <?php if (is_archive()) { ?>
-                                        <h3 class="heading-4 faq__item-name" itemprop="name"><?php echo $question ?></h3>
-                                    <?php } else { ?>
-                                        <div class="heading-4 faq__item-name" itemprop="name"><?php echo $question ?></div>
+                                    <?php
+                                    if ( is_archive() ) {
+                                        ?>
+                                        <h3 class="heading-4 faq__item-name" itemprop="name"><?php echo esc_html( $question ); ?></h3>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <div class="heading-4 faq__item-name" itemprop="name"><?php echo esc_html( $question ); ?></div>
                                     <?php } ?>
                                     <div class="icon-arrow-down faq__item-icon"></div>
                                 </div>
-                            <?php } ?>
-                            <?php if ($answer) { ?>
+                                <?php
+                            endif;
+                            ?>
+                            <?php
+                            if ( $answer ) :
+                                ?>
                                 <div class="faq__item-content accordion__content" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-                                    <span class="text-medium" itemprop="text"><?php echo $answer ?></span>
+                                    <span class="text-medium" itemprop="text"><?php echo esc_html( $answer ); ?></span>
                                 </div>
-                            <?php } ?>
+                                <?php
+                            endif;
+                            ?>
                         </div>
                     <?php } ?>
                 </div>
