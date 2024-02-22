6a0addb8a3d9dbc299c1a2fe7af4afabf26e78e9
Provat <provat@capitalnumbers.com>
2024-02-22 19:10:07 +0530

    Adjusted CSS in various screens as per requested.

--- js/src/common/breakpoints.js
+++ js/src/common/breakpoints.js
@@ -36,16 +36,16 @@ export const PRODUCT_BY_CATEGORY_SLIDER_BREAKPOINTS = {
 // Swiper configuration for vien info block sliders
 export const VIEN_INFO_BLOCK_SLIDER_BREAKPOINTS = {
     480: {
-        perPage: 2,
+        perPage: 1,
     },
     768: {
         perPage: 2,
     },
     1024: {
-        perPage: 4,
+        perPage: 3,
     },
     1280: {
-        perPage: 5,
+        perPage: 4,
     },
     1440: {
         perPage: 6,
