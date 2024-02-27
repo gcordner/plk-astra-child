5b736b433c1b9214b0e82273cec2667c9ce53c34
Provat <provat@capitalnumbers.com>
2024-02-27 14:43:19 +0530

    Added the third conditional block that has small image, adjusted the CSS accordingly.

--- template-parts/image_text.php
+++ template-parts/image_text.php
@@ -41,34 +41,73 @@ if ( $big_image ) :
 	?>
 	</section>
 	<?php
-elseif ( ! $reverse ) :
-	?>
-	<section class="infoblock <?php echo $mobile_image_down ? 'infoblock--about' : ''; ?> section-margin section-padding">
-	<?php
-	if ( $image ) :
+else :
+	if ( ! $reverse ) :
+		?>
+		<section class="infoblock <?php echo $mobile_image_down ? 'infoblock--about' : ''; ?> section-margin section-padding">
+		<?php
+		if ( $image ) :
+			get_template_part(
+				'template-parts/sub-templates/image_text',
+				'picture',
+				array(
+					'image'       => $image,
+					'block_title' => $block_title,
+					'small_image' => $small_image,
+				)
+			);
+		endif;
 		get_template_part(
 			'template-parts/sub-templates/image_text',
-			'picture',
+			'container',
 			array(
 				'image'       => $image,
 				'block_title' => $block_title,
-				'small_image' => $small_image,
+				'text'        => $text,
+				'btn_link'    => $btn_link,
 			)
 		);
+		?>
+		</section>
+		<?php
+	else :
+		?>
+		<section class="aboutinfo section-padding">
+			<div class="ast-container">
+				<div class="container">
+					<div class="aboutinfo__wrapper section-padding-top">
+						<div class="aboutinfo__text">
+							<?php
+							if ( $block_title ) :
+								?>
+								<h2 class="heading-2 aboutinfo__heading"><?php echo esc_html( $block_title ); ?></h2>
+								<h3>Reverse should be True.</h3>
+							<?php
+							endif;
+							?>
+							<?php echo wp_kses_data( str_replace( array( '<p>', '<strong>' ), array( '<p class="infoblock__caption aboutinfo__caption">', '<p class="text-large infoblock__caption aboutinfo__caption">' ), $text ) ); ?>
+						</div>
+						<?php
+						if ( $image ) :
+							?>
+							<picture class="aboutinfo__picture aboutinfo__image">
+								<?php
+								$alt_text = ! empty( $image['alt'] ) ? $image['alt'] : $block_title;
+								echo wp_get_attachment_image( $image['ID'], 'large', '', array( 'alt' => $alt_text ) );
+								?>
+							</picture>
+							<?php
+						endif;
+						if( !empty( $btn_link ) ):
+						?>
+							<a href="<?php echo esc_url( $btn_link['url'] ); ?>" class="btn btn--middle btn--primary infoblock__btn"><?php echo esc_html( $btn_link['title'] ); ?></a>
+						<?php
+						endif;
+						?>
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php
 	endif;
-	get_template_part(
-		'template-parts/sub-templates/image_text',
-		'container',
-		array(
-			'image'       => $image,
-			'block_title' => $block_title,
-			'text'        => $text,
-			'btn_link'    => $btn_link,
-		)
-	);
-	?>
-	</section>
-	<?php
-else:
-
 endif;
